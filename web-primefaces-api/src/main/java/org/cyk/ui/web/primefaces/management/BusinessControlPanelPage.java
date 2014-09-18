ecc8d74de531390ddb5840ac63c9658467f8cfb4
CYK Developper Home <kycdev@gmail.com>
2014-09-18 13:38:42 +0000

    Change-Id: I92470c4501d6211c6e69cb0bf4af1ade9db6a88e

---
+++ web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/management/BusinessControlPanelPage.java
@@ -0,0 +1,46 @@
+package org.cyk.ui.web.primefaces.management;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import lombok.Getter;
+
+import org.cyk.system.root.business.api.BusinessLayer;
+import org.cyk.system.root.business.api.BusinessManager;
+import org.cyk.system.root.business.api.Crud;
+import org.cyk.system.root.business.impl.AbstractBusinessLayer;
+import org.cyk.ui.web.primefaces.AbstractPrimefacesPage;
+import org.cyk.ui.web.primefaces.PrimefacesEditor;
+import org.cyk.utility.common.AbstractMethod;
+
+public class BusinessControlPanelPage extends AbstractPrimefacesPage implements Serializable {
+
+	private static final long serialVersionUID = -3563847253553434464L;
+	
+	@Inject protected BusinessManager businessManager;
+	@Getter private PrimefacesEditor editor;
+	
+	@Getter private List<AbstractBusinessLayer> businessLayers = new ArrayList<>();
+	
+	@Override
+	protected void initialisation() {
+		super.initialisation();
+		title=text("deployment");
+		for(BusinessLayer businessLayer : businessManager.findBusinessLayers())
+			businessLayers.add((AbstractBusinessLayer) businessLayer); 
+		editor = (PrimefacesEditor) editorInstance(new Object(),Crud.READ);
+		editor.getSubmitCommand().setLabel(text("execute"));
+		editor.setSubmitMethodMain(new AbstractMethod<Object, Object>() {
+			private static final long serialVersionUID = -2421175279479434675L;
+			@Override
+			protected Object __execute__(Object parameter) {
+				businessManager.createData();
+				return null;
+			}
+		});
+	}
+
+}
