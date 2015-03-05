ce41fc6dd22bce39b9a81320cc7b874c5a059a5d
CYK Developper Home <kycdev@gmail.com>
2015-03-05 22:19:41 +0000

    Change-Id: I9b4dc1a575b5603f6e002c716a879dbe0a71525c

--- web-api/src/main/java/org/cyk/ui/web/api/AbstractWebManager.java
+++ web-api/src/main/java/org/cyk/ui/web/api/AbstractWebManager.java
@@ -1,9 +1,11 @@
 package org.cyk.ui.web.api;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.cyk.system.root.model.AbstractIdentifiable;
+import org.cyk.ui.api.UIProvider;
 import org.cyk.ui.api.UserSession;
 import org.cyk.ui.api.command.UICommandable;
 import org.cyk.utility.common.cdi.AbstractBean;
@@ -12,6 +14,43 @@ public abstract class AbstractWebManager extends AbstractBean implements Seriali
 
 	private static final long serialVersionUID = 406884223652214395L;
 
+	/**/
+	
+	public Collection<UICommandable> menuItemModules(UserSession userSession){
+		Collection<UICommandable> modules = new ArrayList<>();
+		modules.addAll(menuItemBusinessModules(userSession));
+		modules.add(menuItemControlPanelModule(userSession));
+		modules.add(menuItemUserAccountModule(userSession));
+		modules.add(menuItemHelpModule(userSession));
+		return modules;
+	}
+	
+	public Collection<UICommandable> menuItemBusinessModules(UserSession userSession){
+		Collection<UICommandable> items = new ArrayList<>();
+		
+		return items;
+	}
+	
+	public UICommandable menuItemControlPanelModule(UserSession userSession){
+		UICommandable module = UIProvider.getInstance().createCommandable("command.controlpanel", null);
+		
+		return module;
+	}
+	
+	public UICommandable menuItemUserAccountModule(UserSession userSession){
+		UICommandable module = UIProvider.getInstance().createCommandable("command.account", null);
+		
+		return module;
+	}
+	
+	public UICommandable menuItemHelpModule(UserSession userSession){
+		UICommandable module = UIProvider.getInstance().createCommandable("command.help", null);
+		
+		return module;
+	}
+	
+	/**/
+	
 	public abstract Collection<UICommandable> businessMenuItems(UserSession userSession);
 	
 	public abstract Collection<Class<? extends AbstractIdentifiable>> parameterMenuItemClasses(UserSession userSession);
