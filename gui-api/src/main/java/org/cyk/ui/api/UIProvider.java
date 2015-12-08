68870f01870c4a02ecbbc987bf4760d570934ccb
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2015-12-08 13:16:21 +0000

    Change-Id: I368a272e431c8c1b98164523547235170bd40a37

--- gui-api/src/main/java/org/cyk/ui/api/UIProvider.java
+++ gui-api/src/main/java/org/cyk/ui/api/UIProvider.java
@@ -120,6 +120,7 @@ public class UIProvider extends AbstractBean implements Serializable {
 				if(Boolean.TRUE.equals(loadChoices))
 					for(UIProviderListener<?,?,?,?,?> listener : uiProviderListeners)
 						listener.choices(inputChoice,data,field, (List<Object>) inputChoice.getList());
+				
 			}else if(control instanceof InputFile){
 				InputFile<?,?,?,?,?> inputFile = (InputFile<?,?,?,?,?>)control;
 				org.cyk.utility.common.annotation.user.interfaces.InputFile annotation = inputFile.getField().getAnnotation(org.cyk.utility.common.annotation.user.interfaces.InputFile.class);				
