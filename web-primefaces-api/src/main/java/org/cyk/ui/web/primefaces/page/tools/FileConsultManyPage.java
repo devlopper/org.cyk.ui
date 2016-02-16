127cb2652461b874758884fee36e9c2aed6424ad
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-02-16 16:43:16 +0000

    Change-Id: I09e3adf3966f6fddce787bd3b832e64b599baf7a

---
+++ web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/page/tools/FileConsultManyPage.java
@@ -0,0 +1,48 @@
+package org.cyk.ui.web.primefaces.page.tools;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map.Entry;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.Getter;
+
+import org.cyk.ui.web.primefaces.Exporter;
+import org.cyk.ui.web.primefaces.page.AbstractPrimefacesPage;
+import org.omnifaces.util.Faces;
+
+@Named @ViewScoped @Getter
+public class FileConsultManyPage extends AbstractPrimefacesPage implements Serializable {
+
+	private static final long serialVersionUID = 3274187086682750183L;
+ 
+	private Exporter exporter = new Exporter();
+	
+	@Override
+	protected void initialisation() {
+		super.initialisation();
+		Collection<Object> parametersCollection = new ArrayList<Object>();
+		HttpServletRequest request = Faces.getRequest();
+		for(Entry<String, String[]> entry : request.getParameterMap().entrySet()){
+			parametersCollection.add(entry.getKey());
+			if(entry.getValue()!=null && entry.getValue().length>0)
+				parametersCollection.add(entry.getValue()[0]);
+		}
+		Object[] parametersArray = parametersCollection.toArray();
+		
+		exporter.setFileUrl("/gui-primefaces"+navigationManager.url("imageservlet", parametersArray,Boolean.FALSE,Boolean.TRUE));
+		//exporter.setFileUrl("/gui-primefaces/_cyk_file_image/?identifier=353");
+		exporter.setType("image/jpeg");
+		//exporter.setType("application/pdf");
+		debug(exporter);
+	}
+	
+	@Override
+	public Boolean getShowContentMenu() {
+		return Boolean.FALSE;
+	}
+}
