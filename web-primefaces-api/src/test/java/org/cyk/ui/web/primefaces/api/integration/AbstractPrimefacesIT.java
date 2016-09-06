d032d6bb4a45ea3115d8215890b24ce1c6b46ca8
Christian Yao Komenan <kycdev@gmail.com>
2016-09-06 14:13:45 +0000

    Change-Id: Ib61532ab572552bdcf5763256127a491245cc660

--- web-primefaces-api/src/test/java/org/cyk/ui/web/primefaces/api/integration/AbstractPrimefacesIT.java
+++
@@ -1,8 +0,0 @@
-package org.cyk.ui.web.primefaces.api.integration;
-
-
-public abstract class AbstractPrimefacesIT extends AbstractWebIT {
-
-	private static final long serialVersionUID = 4749522434503605932L;
- 
-}
