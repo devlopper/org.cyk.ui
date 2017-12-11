e5d1aa22e85549f63027bdc131092fb3c006a57d
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2017-12-11 14:11:45 +0000

    Change-Id: I6a7bfd526ff9e7634229632617714bcb8f91e7e6

--- ui-web-primefaces-resources/src/main/java/org/cyk/ui/web/primefaces/resources/LazyDataModel.java
+++ ui-web-primefaces-resources/src/main/java/org/cyk/ui/web/primefaces/resources/LazyDataModel.java
@@ -1,9 +1,11 @@
 package org.cyk.ui.web.primefaces.resources;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.cyk.utility.common.Comparator;
 import org.cyk.utility.common.helper.ThrowableHelper;
 import org.cyk.utility.common.userinterface.Component;
 import org.cyk.utility.common.userinterface.collection.DataTable;
@@ -18,9 +20,24 @@ public class LazyDataModel<T> extends org.primefaces.model.LazyDataModel<DataTab
 		this.component = component;
 	}
 	
-	protected List<T> __load__(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters){
+	protected List<T> __getInstances__(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters){
 		ThrowableHelper.getInstance().throwNotYetImplemented();
 		return null;
+	}
+	
+	protected List<T> __load__(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters){
+		List<T> instances = __getInstances__(first, pageSize, sortField, sortOrder, filters);
+		
+		if(Boolean.TRUE.equals(isFilterable(filters)))
+			instances = filter(instances, filters);
+		
+		if(Boolean.TRUE.equals(isSortable(sortField)))
+			instances = sort(instances, sortField, sortOrder);
+		
+		if(Boolean.TRUE.equals(isPageable(first,pageSize)))
+			instances = page(instances, first,pageSize);
+		
+		return instances;
 	}
 	
 	protected Integer __count__(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters){
@@ -30,9 +47,46 @@ public class LazyDataModel<T> extends org.primefaces.model.LazyDataModel<DataTab
 	
 	@Override
 	public List<DataTable.Row> load(int first, int pageSize, String sortField,SortOrder sortOrder, Map<String, Object> filters) {
-		List<T> instances = (List<T>) __load__(first, pageSize, sortField, sortOrder, filters);
+		List<T> instances = __load__(first, pageSize, sortField, sortOrder, filters);	
 		setRowCount(__count__(first, pageSize, sortField, sortOrder, filters));
-		return (List<DataTable.Row>) DataTable.Row.instanciateMany(instances,component,null);
+		List<DataTable.Row> rows = (List<DataTable.Row>) DataTable.Row.instanciateMany(instances,component,null);
+		if("__orderNumber__".equals(sortField))
+			sort(rows, sortOrder, sortField);
+		return rows;
+	}
+	
+	protected Boolean isFilterable(Map<String, Object> filters){
+		return Boolean.FALSE;
+	}
+	
+	protected List<T> filter(List<T> collection,Map<String, Object> filters){
+		return collection;
+	}
+	
+	protected Boolean isSortable(String fieldName){
+		return Boolean.FALSE;
+	}
+	
+	protected List<T> sort(List<T> collection,String sortField,SortOrder sortOrder){
+		sort(collection, sortOrder,sortField);
+		return collection;
+	}
+	
+	protected Boolean isPageable(Integer first,Integer size){
+		return Boolean.FALSE;
+	}
+	
+	protected List<T> page(List<T> collection,Integer first,Integer size){
+		return collection.subList(first, first+size);
 	}
 
+	/**/
+	
+	protected static <T> Comparator<T> instanciateComparator(SortOrder sortOrder,String...fieldNames){
+		return new Comparator<T>(SortOrder.ASCENDING.equals(sortOrder) ? Comparator.Order.ASCENDING : Comparator.Order.DESCENDING,fieldNames);
+	}
+	
+	protected static <T> void sort(List<T> list,SortOrder sortOrder,String...fieldNames){
+		Collections.sort(list, instanciateComparator(sortOrder,fieldNames));
+	}
 }
