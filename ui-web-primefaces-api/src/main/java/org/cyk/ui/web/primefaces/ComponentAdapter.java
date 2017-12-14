5539d81209bc20bbbaf3678179e008f2414a4b99
Christian Yao Komenan <Christian@CYK-HP-LAPTOP>
2017-12-14 14:24:45 +0000

    Change-Id: I535a62d4a56596b69b6a1773655e27f6ccb44a65

--- ui-web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/ComponentAdapter.java
+++ ui-web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/ComponentAdapter.java
@@ -15,28 +15,7 @@ public class ComponentAdapter extends org.cyk.ui.web.primefaces.resources.Compon
 		Object object = super.build(component);
 		if(component instanceof DataTable){
 			if(Boolean.TRUE.equals(component.getPropertiesMap().getLazy())){
-				component.getPropertiesMap().setValue(new LazyDataModel<AbstractIdentifiable>(component){
-					private static final long serialVersionUID = 1L;
-					
-					/*@Override
-					protected List<AbstractIdentifiable> __getInstances__(int first, int pageSize, String sortField, SortOrder sortOrder,Map<String, Object> filters) {
-						Class<AbstractIdentifiable> aClass = (Class<AbstractIdentifiable>) component.getPropertiesMap().getActionOnClass();
-						FilterHelper.Filter<AbstractIdentifiable> filter = (Filter<AbstractIdentifiable>) ClassHelper.getInstance().instanciateOne(FilterClassLocator.getInstance().locate(aClass));
-						filter.set((String)filters.get("globalFilter"));
-						DataReadConfiguration dataReadConfiguration = new DataReadConfiguration(new Long(first), new Long(pageSize));
-						return (List<AbstractIdentifiable>) inject(BusinessInterfaceLocator.class).injectTyped(aClass).findByFilter(filter, dataReadConfiguration);
-					}*/
-					
-					/*@Override
-					protected Integer __count__(Collection<AbstractIdentifiable> instances, int first, int pageSize,String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-						Class<AbstractIdentifiable> aClass = (Class<AbstractIdentifiable>) component.getPropertiesMap().getActionOnClass();
-						FilterHelper.Filter<AbstractIdentifiable> filter = (Filter<AbstractIdentifiable>) ClassHelper.getInstance().instanciateOne(FilterClassLocator.getInstance().locate(aClass));
-						filter.set((String)filters.get("globalFilter"));
-						DataReadConfiguration dataReadConfiguration = new DataReadConfiguration(new Long(first), new Long(pageSize));
-						return inject(BusinessInterfaceLocator.class).injectTyped(aClass).countByFilter(filter, dataReadConfiguration).intValue();
-					}*/
-					
-				});
+				component.getPropertiesMap().setValue(new LazyDataModel<AbstractIdentifiable>(component));
 			}
 		}
 		return object;
