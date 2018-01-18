be35cbcef6f2ce13ed17c69a51e8464a60c93cfb
Christian Yao Komenan <kycdev@gmail.com>
2018-01-18 15:42:24 +0000

    Change-Id: Id4e9c9961db8b0a43b94b8e06db83855989d1e41

--- ui-web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/MenuBuilder.java
+++ ui-web-primefaces-api/src/main/java/org/cyk/ui/web/primefaces/MenuBuilder.java
@@ -4,10 +4,19 @@ import java.io.Serializable;
 
 import org.cyk.system.root.model.event.Event;
 import org.cyk.system.root.model.file.File;
+import org.cyk.system.root.model.generator.StringGenerator;
 import org.cyk.system.root.model.geography.ContactCollection;
+import org.cyk.system.root.model.information.IdentifiableCollection;
+import org.cyk.system.root.model.language.Language;
 import org.cyk.system.root.model.mathematics.Movement;
-import org.cyk.system.root.model.party.person.Person;
-import org.cyk.utility.common.helper.ClassHelper;
+import org.cyk.system.root.model.message.SmtpProperties;
+import org.cyk.system.root.model.network.Computer;
+import org.cyk.system.root.model.party.Party;
+import org.cyk.system.root.model.pattern.tree.DataTree;
+import org.cyk.system.root.model.security.Role;
+import org.cyk.system.root.model.time.Period;
+import org.cyk.system.root.model.userinterface.ClassUserInterface;
+import org.cyk.system.root.model.value.Value;
 import org.cyk.utility.common.userinterface.command.Menu;
 
 public class MenuBuilder extends org.cyk.ui.web.primefaces.resources.MenuBuilder implements Serializable {
@@ -82,7 +91,10 @@ public class MenuBuilder extends org.cyk.ui.web.primefaces.resources.MenuBuilder
 	@Override
 	protected void addNodeIdentifiablesManage(Menu menu) {
 		super.addNodeIdentifiablesManage(menu);
-		menu.addNodeActionListManyFromPackage(ContactCollection.class,Event.class,File.class,Person.class,Movement.class);
+		menu.addNodeActionListManyFromPackage(ContactCollection.class,Event.class,File.class,Party.class,Movement.class
+				,Value.class
+				,ClassUserInterface.class,Period.class,Role.class,DataTree.class,Computer.class,SmtpProperties.class,Language.class
+				,IdentifiableCollection.class,StringGenerator.class);
 	}
 	
 }
