758a8bee5fbb02f12db529dc4bbc11cf1eba2fdc
Christian Yao Komenan <kycdev@gmail.com>
2018-01-23 17:23:23 +0000

    Change-Id: Ia24ff1cb6b1f8da86cdb56f14552b8c879dc00b0

--- ui-web-primefaces-resources/src/main/java/org/cyk/ui/web/primefaces/resources/ServletContextListener.java
+++ ui-web-primefaces-resources/src/main/java/org/cyk/ui/web/primefaces/resources/ServletContextListener.java
@@ -2,18 +2,15 @@ package org.cyk.ui.web.primefaces.resources;
 
 import java.io.Serializable;
 
-import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 
 public class ServletContextListener extends org.cyk.ui.web.api.resources.ServletContextListener implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Inject protected PrimefacesResourcesManager primefacesResourcesManager;
-	
 	@Override
 	public void __contextInitialized__(ServletContextEvent servletContextEvent) {
 		super.__contextInitialized__(servletContextEvent);
-		primefacesResourcesManager.initializeContext(servletContextEvent);
+		inject(PrimefacesResourcesManager.class).initializeContext(servletContextEvent);
 	}
 	
 }
